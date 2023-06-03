4774021b728171590c4806a6dc47c909a0a59372
aleksandrdiptan <diptan@huntflow.ru>
2023-06-03 15:41:33 +0300

    add russian name for all fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,30 +3,32 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    title = models.CharField(verbose_name="Имя на русском", max_length=200)
+    description = models.TextField(verbose_name="Описание", null=True)
     previous_evolution = models.ForeignKey("self",
                                            on_delete=models.CASCADE,
                                            null=True,
                                            blank=True,
                                            related_name="next_evolution",
+                                           verbose_name="Предыдущая эволюция",
                                            )
-    title_en = models.CharField(null=True, max_length=200)
-    title_jp = models.CharField(null=True, max_length=200)
-    image = models.ImageField(null=True)
+    title_en = models.CharField(verbose_name="Имя на английском", null=True, max_length=200)
+    title_jp = models.CharField(verbose_name="Имя на японском", null=True, max_length=200)
+    image = models.ImageField(verbose_name="Картинка", null=True)
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon_id = models.ForeignKey(Pokemon, on_delete=models.CASCADE, blank=True, default=1)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=django.utils.timezone.now())
-    disappeared_at = models.DateTimeField(default=django.utils.timezone.now())
-    level = models.IntegerField(blank=True, null=True)
-    health = models.IntegerField(blank=True, null=True)
-    strength = models.IntegerField(blank=True, null=True)
-    defence = models.IntegerField(blank=True, null=True)
-    stamina = models.IntegerField(blank=True, null=True)
+    pokemon_id = models.ForeignKey(Pokemon, on_delete=models.CASCADE, blank=True, default=1,
+                                   verbose_name="Покемон",)
+    lat = models.FloatField(verbose_name="Широта",)
+    lon = models.FloatField(verbose_name="Долгота",)
+    appeared_at = models.DateTimeField(verbose_name="Время появления", default=django.utils.timezone.now())
+    disappeared_at = models.DateTimeField(verbose_name="Время исчезновения", default=django.utils.timezone.now())
+    level = models.IntegerField(verbose_name="Уровень", blank=True, null=True)
+    health = models.IntegerField(verbose_name="Здоровье", blank=True, null=True)
+    strength = models.IntegerField(verbose_name="Сила", blank=True, null=True)
+    defence = models.IntegerField(verbose_name="Атака", blank=True, null=True)
+    stamina = models.IntegerField(verbose_name="Выносливость", blank=True, null=True)
